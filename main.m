42ae9cf0b766abba12d763a9df54e0eb0ead58e1
Amalvir <samalonsovirissel@gmail.com>
2021-03-09 15:38:22 +0100

    La courbe COR expérimentale marche

--- main.m
+++ main.m
@@ -3,14 +3,14 @@ clear
 N = 10000;
 K = 1;
 theta = 3.3;
-p = 1.5;
+param = 1.5;
 
-Y = generer(theta,p,N,K);
+Y = generer(theta,param,N,K);
 histfit(Y,[],'weibull')
 moy_cal = mean(Y);
 var_cal = var(Y);
-moy_exact = theta*gamma(1+1/p);
-var_exact = theta^2*gamma(1+2/p) - moy_exact^2;
+moy_exact = theta*gamma(1+1/param);
+var_exact = theta^2*gamma(1+2/param) - moy_exact^2;
 
 %2nd test
 clear
@@ -18,9 +18,9 @@ clear
 N = 1000;
 K = 500;
 theta = 3.3;
-p = 1.5;
+param = 1.5;
 
-Y = generer(theta,p,N,K);
+Y = generer(theta,param,N,K);
 plot(Y(:,1))
 moy_cal = mean(Y).';
 var_cal = var(Y).';
@@ -30,18 +30,18 @@ plot(abscisses, moy_cal, abscisses, sqrt(var_cal))
 
 % 2.Estimateur
 
-[alpha_est,BRC] = estimateur_mv(Y,p,N);
+[alpha_est,BRC] = estimateur_mv(Y,param,N);
 disp(['BRC = ', num2str(BRC)])
 disp(['moyenne = ', num2str(mean(alpha_est))])
 disp(['Variance = ', num2str(var(alpha_est))]) % Reste à comparer avec les valeurs théoriques
-disp(['moyenne exatcte = ', num2str(theta^p)])
-disp(['Variance = ', num2str(theta^p^2/N)])
+disp(['moyenne exatcte = ', num2str(theta^param)])
+disp(['Variance = ', num2str(theta^param^2/N)])
 % 3. Détection
 
 % 3.1 Théorique
 a0 = 0.9;
 a1 = 1.5;
-Y = generer(theta,p,N,K);
+Y = generer(theta,param,N,K);
 alpha = (0.01:0.01:0.99);
 figure
 for N=[10,20,50]
@@ -64,8 +64,12 @@ hold off
 legend('a1=1.2','a1=1.5','a1=2')
 
 %3.2 Pratique
-
-p = pi_estimee(a0,a1,N,K,alpha);
+N = 20;
+K = 50000;
+a0 = 0.9;
+a1 = 1.5;
+figure('Name','Pratique')
+p = pi_estimee(a0,a1,param,N,K,alpha);
 plot(alpha,p)
 
 
@@ -93,14 +97,20 @@ function p = pi_theorique(a0, a1, L, alpha)
     p = 1 - chi2cdf(2*lambda/a1,L);
 end
 
-function T = generer_H1(a1,N,K)
-    theta = a1^(1/p);
-    T = generer(theta,p,N,K);
+function T = generer_H1(a1,param,N,K)
+    theta = a1^(1/param);
+    T = generer(theta,param,N,K);
 end
 
-function p = pi_estimee(a0,a1,N,K,alpha)
+function p = pi_estimee(a0,a1,param,N,K,alpha)
     lambda = a0/2*chi2inv(1-alpha,2*N);
-    T = generer_H1(a1,N,K);
-    R = sum(T) > lambda;
+    T = generer_H1(a1,param,N,K);
+    S = sum(T);
+    R = zeros(K,length(alpha));
+    for i=1:K
+        for j=1:length(alpha)
+            R(i,j) = lambda(j) < S(i);
+        end
+    end
     p = mean(R);
 end
